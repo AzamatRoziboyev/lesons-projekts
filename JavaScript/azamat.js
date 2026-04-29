19847fc6a8f73842caf01ba8335f8ac2c20db631
AzamatRoziboyev <aroziboyev1066@gmail.com>
2026-04-29 17:24:18 +0500

    java

---
+++ JavaScript/azamat.js
@@ -0,0 +1,21 @@
+let birinchiSon = 3
+let ikkinchiSon = 4
+let uchinchiSon = 7
+
+let ayirish = birinchiSon - ikkinchiSon
+let qoshish = ikkinchiSon + uchinchiSon
+let hamma_Yigindi = birinchiSon + ikkinchiSon + uchinchiSon
+
+console.log("Ayirish", ayirish)
+console.log("qoshish", qoshish)
+console.log("Hammasi qoshish", hamma_Yigindi)
+
+
+console.log("nagapla")
+
+let ism = "azamat"
+let familiya = "roziboyev"
+
+let ismlarni_qoshish = ism + familiya
+
+console.log("Qoshish", ismlarni_qoshish)
