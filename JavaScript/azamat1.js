f5931475426c5a5fed962150147f98c90df613ee
AzamatRoziboyev <aroziboyev1066@gmail.com>
2026-05-04 16:17:58 +0500

    java

---
+++ JavaScript/azamat1.js
@@ -0,0 +1,18 @@
+let talabaIsmi = "Azamat"
+talabaIsmi = "borya"
+talabaIsmi = "Suxrob"
+console.log(talabaIsmi)
+
+
+const yosh = 15
+// yosh = 16
+console.log(yosh)
+
+let age = 16
+
+if (age > 18) {
+    console.log("siz balogat yoshiga yetmagansz")
+}
+else {
+    console.log("siz balogat yoshiga yetgansz")
+}
