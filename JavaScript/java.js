7132dec42564507bcfa72afb2ac65c49a77319f1
AzamatRoziboyev <aroziboyev1066@gmail.com>
2026-05-04 16:49:21 +0500

    java

---
+++ JavaScript/java.js
@@ -0,0 +1,33 @@
+// ====operatorlar=======
+
+
+let son1 = 13
+let son2 = 4
+let qoshish = son1 + son2
+console.log(qoshish);
+
+let ayirish = son1 - son2
+console.log(ayirish);
+
+let kopaytirish = son1 * son2
+console.log(kopaytirish);
+
+let bolish = son1 / son2
+console.log(bolish);
+
+let daraja = son1 ** son2
+console.log(daraja);
+
+let qoldiq = son1 % son2
+console.log(qoldiq);
+
+//  birga oshirish va kamayish
+
+console.log(son1++);
+// console.log(son1--)
+console.log(son1);
+
+
+
+
+
