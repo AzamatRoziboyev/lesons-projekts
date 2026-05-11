9fa61fe37355c24682e1dcca19c36164e1165e01
AzamatRoziboyev <aroziboyev1066@gmail.com>
2026-05-11 16:46:53 +0500

    java

---
+++ data-types.js
@@ -0,0 +1,10 @@
+let son = 10
+console.log(typeof(son));
+
+let text = "20"
+console.log(typeof(text));
+
+let ozgaruvchi = "Shaxruz1804744"
+console.log(typeof(ozgaruvchi));
+
+console.log(son + text, typeof(son + text));
